0d7e3258f48f19d16c1e224413e078f3a82a8429
Tim Hazlett <47572900+thazlett16@users.noreply.github.com>
2020-10-07 00:45:07 -0500

    IP config
    Added function to configure and grab the IP address of the local device and then prints address to the screen.

--- localClient/client.py
+++ localClient/client.py
@@ -1,7 +1,19 @@
 from vidgear.gears import NetGear
+import socket
 import cv2
 
-client_ip_address = "192.168.0.162"
+
+def getDeviceIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("10.255.255.255", 1))
+    IP = s.getsockname()[0]
+    s.close()
+    return IP
+
+
+client_ip_address = getDeviceIP()
+print("client_ip_address", client_ip_address)
+
 options = {'flag': 0, 'copy': False, 'track': False}
 client = NetGear(address=client_ip_address, port='5454', receive_mode=True)
 while True:
--- piServer/server.py
+++ piServer/server.py
@@ -3,8 +3,8 @@ from vidgear.gears import NetGear
 import cv2
 
 client_ip_address = "192.168.0.162"
-options = {"hflip": True, "exposure_mode": "auto", "iso": 800, "exposure_compensation": 15, "awb_mode": "horizon", "sensor_mode": 0}
-stream = PiGear(resolution=(640, 480), framerate=30, logging=True, **options).start()
+options = {"hflip": True, "exposure_mode": "auto", "iso": 1200, "exposure_compensation": 15, "awb_mode": "horizon", "sensor_mode": 0}
+stream = PiGear(resolution=(640, 480), framerate=60, logging=True, **options).start()
 server = NetGear(address=client_ip_address, port='5454')
 
 # loop over until KeyBoard Interrupted
