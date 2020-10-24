7fe3446549f0b01c0270a6165a88a1cd660a091b
Tim Hazlett <timhazlett2016@gmail.com>
2020-10-24 17:57:31 -0500

    Update to server
    Merging our changes in main branch

--- piServer/server.py
+++ piServer/server.py
@@ -1,9 +1,7 @@
 import argparse
 import time
+import zmq
 import picamera
-import numpy as np
-import cv2
-import socket
 import struct
 import io
 
@@ -26,7 +24,7 @@ class customActions(argparse.Action):
 option_parse = argparse.ArgumentParser(description="Set up of Raspberry Pi Camera streaming module")
 option_parse.add_argument("--resolution", type=str, default=low, help="Set the resolution of the camera", choices=["high", "low"], action=customActions)
 option_parse.add_argument("--ip", type=str, default="localhost", help="Set the IP address")
-option_parse.add_argument("--port", type=int, default="5700", help="Set the IP address")
+option_parse.add_argument("--port", type=int, default="5454", help="Set the IP address")
 option_parse.add_argument("--framerate", type=int, default="30", help="Set the framerate")
 option_parse.add_argument("--debug", type=bool, default=False, help="Shows debugging information", choices=[True, False])
 
@@ -41,63 +39,30 @@ if DEBUG:
 
 (height, width) = option_parse.parse_args().resolution
 
-unity_socket = socket.socket()
-#unity_socket.connect((option_parse.parse_args().ip, option_parse.parse_args().port))
-#connection = unity_socket.makefile('wb')
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.set_hwm(1)
+socket.bind("tcp://*:" + str(option_parse.parse_args().port))
 
 camera = picamera.PiCamera()
 camera.resolution = option_parse.parse_args().resolution
 camera.framerate = option_parse.parse_args().framerate
-
+# possibly remove preview?
+camera.start_preview()
 stream = io.BytesIO()
-count = 0
-time.sleep(.2)
+
+time.sleep(.1)
+
+count_frame = 0
 try:
     for frame in camera.capture_continuous(stream, format="jpeg", use_video_port=True):
-        print(stream.getvalue())
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        count = count + 1
-        if count == 5:
-            break
+        message = str(stream.getvalue())
+        socket.send(stream.getvalue())
+        if DEBUG:
+            print("Sent!\n", framenum)
+        framenum = (framenum + 1) % 100
+        stream.seek(0)
 finally:
     camera.close()
     stream.close()
-    unity_socket.close()
-
-"""
-cap = cv2.VideoCapture(0)
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-
-    # Display the resulting frame
-    nparr = frame
-    img_byte = cv2.imencode(".jpg", nparr)[1].tostring()
-    print(type(img_byte))
-    print(img_byte)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-
-
-client_ip_address = "192.168.0.162"
-options = {"hflip": True, "exposure_mode": "auto", "iso": 1500, "exposure_compensation": 15, "awb_mode": "horizon", "sensor_mode": 0}
-stream = PiGear(resolution=(640, 480), framerate=60, logging=True, **options).start()
-server = NetGear(address=client_ip_address, port='5454')
-
-# loop over until KeyBoard Interrupted
-while True:
-    try:
-        frame = stream.read()
-        # add processing ai stuff here
-        server.send(frame)
-    except KeyboardInterrupt:
-        break
-stream.release()
-server.close()
-"""
+    socket.close()
